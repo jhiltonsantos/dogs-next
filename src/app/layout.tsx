4e2de3073d65540c3c6a468d4ab66b37f2b404fc
jhiltonsantos <jhilton930@gmail.com>
2024-05-28 18:50:23 -0300

    Add(DogsNext): create project and configs

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Dogs Next",
+  description: "Rede social para cachorros",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
